4d225ce535fc80df7db94cf9eb438fbdfbc6623f
Mark Johnston <markjdb@gmail.com>
2018-11-30 17:10:09 -0500

    Use $curthread() to get the current vnet.

--- kgdb/vimage2.py
+++ kgdb/vimage2.py
@@ -2,15 +2,19 @@ import gdb
 
 class vimage(gdb.Function):
     def __init__(self):
-        super(vimage, self).__init__("vimage")
+        super(vimage, self).__init__("V")
 
-    def invoke(self, name, vnet=gdb.parse_and_eval('prison0.pr_vnet')):
-        name = name.string()
-        if name.startswith("V_"):
-            name = name[len("V_"):]
+    def invoke(self, sym, vnet=gdb.parse_and_eval("$curthread()->td_vnet")):
+        sym = sym.string()
+        if sym.startswith("V_"):
+            sym = sym[len("V_"):]
+        if gdb.lookup_symbol("sysctl___kern_features_vimage")[0] is None:
+            return gdb.parse_and_eval(sym)
+        if not vnet:
+            vnet = gdb.lookup_global_symbol("vnet0").value()
         base = vnet['vnet_data_base']
-        entry = gdb.lookup_global_symbol('vnet_entry_' + name).value()
-        entry_addr = entry.address.cast(gdb.lookup_type('uintptr_t'))
+        entry = gdb.lookup_global_symbol("vnet_entry_" + sym).value()
+        entry_addr = entry.address.cast(gdb.lookup_type("uintptr_t"))
         return gdb.Value(base + entry_addr).cast(entry.type.pointer())
 
 vimage()
